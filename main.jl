8d1731e611bd2d2a71d2da5c83d9297a39574b07
Ryan Dwyer <rydwy01@stanford.edu>
2024-01-30 20:10:19 -0800

    testing github

--- main.jl
+++ main.jl
@@ -1,21 +1,17 @@
 using POMDPs, QuickPOMDPs, NativeSARSOP, POMDPModels, POMDPTools, QMDP
 
 m = QuickPOMDP(
-    states = ["left", "right"],
-    actions = ["left", "right", "listen"],
-    observations = ["left", "right"],
-    initialstate = Uniform(["left", "right"]),
-    discount = 0.95,
-
-    transition = function (s, a)
+    states=["left", "right"],
+    actions=["left", "right", "listen"],
+    observations=["left", "right"],
+    initialstate=Uniform(["left", "right"]),
+    discount=0.95, transition=function (s, a)
         if a == "listen"
             return Deterministic(s) # tiger stays behind the same door
         else # a door is opened
             return Uniform(["left", "right"]) # reset
         end
-    end,
-
-    observation = function (a, sp)
+    end, observation=function (a, sp)
         if a == "listen"
             if sp == "left"
                 return SparseCat(["left", "right"], [0.85, 0.15]) # sparse categorical distribution
@@ -25,9 +21,7 @@ m = QuickPOMDP(
         else
             return Uniform(["left", "right"])
         end
-    end,
-
-    reward = function (s, a)
+    end, reward=function (s, a)
         if a == "listen"
             return -1.0
         elseif s == a # the tiger was found
@@ -38,11 +32,13 @@ m = QuickPOMDP(
     end
 )
 
+#test
+
 solver = SARSOPSolver()
 policy = solve(solver, m)
 
 rsum = 0.0
-for (s,b,a,o,r) in stepthrough(m, policy, "s,b,a,o,r", max_steps=10)
+for (s, b, a, o, r) in stepthrough(m, policy, "s,b,a,o,r", max_steps=10)
     println("s: $s, b: $([s=>pdf(b,s) for s in states(m)]), a: $a, o: $o")
     global rsum += r
 end
